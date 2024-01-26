17ff6772aed7eae172cb365dc9c36e66e6dd0a1e
Garry <tahinjanaharygarry@gmail.com>
2024-01-26 22:39:58 +0300

    setup container DI

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-app = FastAPI(title="Social Media APP")
+from src.config.app import AppConfig
+from src.config.container import Container
+from src.routers.user import user_router
+
+
+def create_app() -> FastAPI:
+    container = Container()
+
+    api = FastAPI(title="Social Media APP")
+    api.container = container
+    api.include_router(user_router)
+    return api
+
+
+app = create_app()
+config = AppConfig()
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": config.db_url
+    },
+    "apps": {
+        "models": {
+            "models": ["src.models.user", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
 
 register_tortoise(
     app,
-    db_url="sqlite://:memory:",
-    modules={"models": ["models"]},
+    config=TORTOISE_ORM,
     generate_schemas=True,
     add_exception_handlers=True,
 )
